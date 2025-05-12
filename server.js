e33cd67a9a9f0607c3675d706e4f591e15cdf45c
RJ <fredrickalexander1727@gmail.com>
2025-05-11 21:15:11 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import hockeyRouter from './routes/hockeyRouter.js';
+
+const app = express();
+const PORT = 1917;
+
+app.set('view engine','pug');
+app.use(express.static('public'));
+
+app.use('/', hockeyRouter);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
